78126abe49478c2fbf37cddd2825300b3972eab2
Aleksandr Vorobei <vorobeisasa@gmail.com>
2025-04-22 20:04:36 +0300

    start project

---
+++ AezakmiEditPhoto/AezakmiEditPhoto/AezakmiEditPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AezakmiEditPhotoApp.swift
+//  AezakmiEditPhoto
+//
+//  Created by Александр Воробей on 22.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct AezakmiEditPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
